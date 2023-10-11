20e5d03e2d35b145e333bc69a5681dbdd09f2b1e
m-mueller678 <marcus.mueller.678@gmail.com>
2023-10-11 19:14:24 +0200

    test framework

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -15,5 +34,19 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    {
+        test_main();
+        unreachable!()
+    }
     panic!("Some panic message");
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
 }
