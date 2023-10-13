d2c9a96a49fee8e22cb7f6011b8a366e4816023a
m-mueller678 <marcus.mueller.678@gmail.com>
2023-10-13 18:15:51 +0200

    simple executor

---
+++ src/task/mod.rs
@@ -0,0 +1,24 @@
+use alloc::boxed::Box;
+use core::{future::Future, pin::Pin};
+
+pub mod simple_executor;
+
+pub struct Task {
+    future: Pin<Box<dyn Future<Output = ()>>>,
+}
+
+impl Task {
+    pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
+        Task {
+            future: Box::pin(future),
+        }
+    }
+}
+
+use core::task::{Context, Poll};
+
+impl Task {
+    fn poll(&mut self, context: &mut Context) -> Poll<()> {
+        self.future.as_mut().poll(context)
+    }
+}
