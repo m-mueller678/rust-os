67b272e2c7fe7e42a793ea64d851b42056cc9f70
m-mueller678 <marcus.mueller.678@gmail.com>
2023-11-19 21:27:29 +0100

    gdb debugging notes

--- run-qemu.sh
+++ run-qemu.sh
@@ -6,3 +6,8 @@ cd ..
 cargo run -- target/x86_64-unknown-none/debug/kernel os.img
 
 qemu-system-x86_64 -enable-kvm -drive format=raw,file=os.img -display none -serial stdio
+
+
+# INFO : virtual_address_offset: XXX
+# target remote :1234
+# symbol-file -o 0x8000000000 target/x86_64-unknown-none/debug/kernel
